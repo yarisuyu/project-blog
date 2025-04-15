2ffae9fa1f46d468c2bc119fa4e1f34c8af302b8
Elena Oblomova <lisinaeg@gmail.com>
2025-04-15 12:49:03 +0300

    Exercise 1: Homepage list of posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,39 @@
 import React from 'react';
 
+import { getBlogPostList } from '../helpers/file-helpers';
 import BlogSummaryCard from '@/components/BlogSummaryCard';
 
 import styles from './homepage.module.css';
 
-function Home() {
+async function Home() {
+
+  const blogPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
 
-      {/* TODO: Iterate over the data read from the file system! */}
+      {blogPosts.map(blogPost => (
+        <BlogSummaryCard
+            key={crypto.randomUUID()}
+            slug={blogPost.slug}
+            title={blogPost.title}
+            abstract={blogPost.abstract}
+            publishedOn={blogPost.publishedOn}
+          />
+      )
+      )}
+
       <BlogSummaryCard
+        key={crypto.randomUUID()}
         slug="example"
         title="Hello world!"
         abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
         publishedOn={new Date()}
       />
+
     </div>
   );
 }
